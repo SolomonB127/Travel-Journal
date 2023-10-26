be5053ee6bc540044302e62f80c9c840fbfef9cf
SolomonB127 <solomonakinsulire@gmail.com>
2023-10-26 09:25:15 +0100

    Passed props to Card.jsx file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import pin from './Images/pin.svg'
 const Card = (props) => {
   return (
     <section className='Card--container'>
       <div className='cards'>
-        
+        <img src= {props.item.cardImg} alt=""  width={300}/> 
+        <div>
+          <img src= {pin} alt="" />
+          <h4>{props.item.Location}</h4> 
+          <span>View on Google Map Link</span>
+          <h1>{props.item.title}</h1>
+          <h5>{props.item.timePeriod}</h5>
+          <p>{props.item.description}</p>
+        </div>
+        <hr />  
       </div>
     </section>
   )
