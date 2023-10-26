3cf54f4aa871fa270da595ca26a8f4189fc6bb73
SolomonB127 <solomonakinsulire@gmail.com>
2023-10-26 08:57:56 +0100

    Data added in Data.js file

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import Abuja from './components/Images/Abuja Gate.jpg'
+import Arc from './components/Images/Arc de Triomphe.jpg'
+import Rome from './components/Images/Colosseum of Rome.jpg'
+import Japan from './components/Images/mt-fuji.jpg'
+export default[
+    {
+        id: 1,
+        cardImg: Abuja,
+        Location: "Nigeria",
+        title: "Abuja City Gate",
+        description: "Something",
+        timePeriod: "26 Oct, 2023 - 10 Nov, 2023"
+    },
+    {
+        id: 2,
+        cardImg: Arc,
+        Location: "France",
+        title: "Arc de Triomphe",
+        description: "Something",
+        timePeriod: "15 Nov, 2023 - 30 Nov, 2023"
+    },
+    {
+        id: 3,
+        cardImg: Rome,
+        Location: "Rome",
+        title: "Colosseum of Rome",
+        description: "Something",
+        timePeriod: "1 Dec, 2023 - 16 Dec, 2023"
+    },
+    {
+        id: 4,
+        cardImg: Japan,
+        Location: "Japan",
+        title: "Mt. Fuji",
+        description: "Something",
+        timePeriod: "18 Dec, 2023 - 26 Dec, 2023"
+    }
+]
